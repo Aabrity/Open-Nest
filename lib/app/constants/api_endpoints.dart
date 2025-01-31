f19b702a1b6b1983c97f388e07f8cf757e1dd744
Aabrity <aabrityd@gmail.com>
2025-01-31 06:16:43 +0545

    changes in end point

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -3,20 +3,20 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/";
+  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
   // For iPhone
   //static const String baseUrl = "http://localhost:3000/api/";
 
   // ====================== Auth Routes ======================
-  static const String login = "auth/signin";
-  static const String register = "auth/signup";
+  static const String login = "auth/login";
+  static const String register = "auth/register";
   // static const String getAllStudent = "auth/getAllStudents";
   // static const String getStudentsByBatch = "auth/getStudentsByBatch/";
   // static const String getStudentsByCourse = "auth/getStudentsByCourse/";
   static const String updateUser = "auth/updateUser/";
   static const String deleteUser = "auth/deleteUser/";
   static const String imageUrl = "http://10.0.2.2:3000/uploads/";
-  static const String uploadImage = "user/uploadImage";
+  static const String uploadImage = "auth/uploadImage";
 
   // ====================== Batch Routes ======================
   // static const String createBatch = "batch/createBatch";
